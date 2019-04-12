a8d7f5292383938ea24dba3eb3e68a5666f3aea0
maina murage <mainamurage21@gmail.com>
2019-04-12 11:55:12 +0300

    Create Source class and test the source class
    the Source class is created and we test for the source object
    add an __init__ file for the model folder
    add minor change to the app __init__  file

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 
 #initialize app
-app = Flask(__name__, instance_relative_config= True)
+app = Flask(__name__, instance_relative_config = True)
 
 #set up configuration 
 app.config.from_object(DevConfig)
