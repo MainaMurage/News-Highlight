0ed193ea04fa1eee32467c4e5411b3ad6d074ccd
maina murage <mainamurage21@gmail.com>
2019-04-12 14:26:32 +0300

    Update request.py to process requests & responses
    add functions to request sources data from the api and process them
    to make them readable for our flask app

--- app/request.py
+++ app/request.py
@@ -1,4 +1,50 @@
 from app import app 
+#import the module that will help create a connection to the API URL and send a request
+#import json modules that will format the JSON response to a dict
+import urllib.request, json
+from .models import source
+
+Source = source.Source
 
 #get the api key
 api_key =app.config['NEWS_HIGHLIGHT_API_KEY']
+
+#get the source base url
+base_url = app.config["NEWS_HIGHLIGHT_BASE_URL"]
+
+def get_sources() :
+  '''
+  get the json response to our url request 
+  '''
+  get_sources_url = base_url.format(api_key)
+
+  with urllib.request.urlopen(get_sources_url) as url :
+    get_sources_data = url.read()
+    get_sources_response = json.loads(get_sources_data)
+
+    source_results =  None 
+
+    if get_sources_response['results'] :
+      source_results_list = get_sources_response['results']
+      source_results = process_results(source_results_list)
+
+  return source_results 
+
+def process_results(source_list) :
+    '''
+    process source result and transform them to a list of objects
+    '''
+    source_results = []
+    for source_item in source_list :
+      id = source_item.get('id')
+      name = source_item.get('name')
+      description = source_item.get('description')
+      url = source_item.get('url')
+      category = source_item.get('category')
+      language = source_item.get('language')
+      country = source_item.get('country')
+
+      source_object = Source(id, name, description, url, category, language, country)
+      source_results.append(source_object)
+
+    return source_results
